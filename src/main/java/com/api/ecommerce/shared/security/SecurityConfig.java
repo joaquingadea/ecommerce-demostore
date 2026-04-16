2f702cf73c1bb5a68f555c59259e484e7b8a5a0f
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-15 22:05:51 -0300

    Implementación de SecurityFilterChain
    SecurityConfig.java

--- src/main/java/com/api/ecommerce/shared/security/SecurityConfig.java
+++ src/main/java/com/api/ecommerce/shared/security/SecurityConfig.java
@@ -1,4 +1,25 @@
 package com.api.ecommerce.shared.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig{
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        return http.csrf(AbstractHttpConfigurer::disable)
+                //.cors(config -> config.disable()) CONFIGURAR
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
+    }
 }
