c2fbb24b8149e35b1627fba7a83822ad4b5276ff
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-23 20:46:20 -0300

    Agregado de método getSinglePageProductById
    IProductService.java

--- src/main/java/com/api/ecommerce/products/application/IProductService.java
+++ src/main/java/com/api/ecommerce/products/application/IProductService.java
@@ -1,7 +1,10 @@
 package com.api.ecommerce.products.application;
 
 import com.api.ecommerce.products.dto.request.CreateProductRequestDTO;
+import com.api.ecommerce.products.dto.response.ProductSinglePageDTO;
+import org.jspecify.annotations.Nullable;
 
 public interface IProductService {
     void create(CreateProductRequestDTO requestDTO);
+    ProductSinglePageDTO getSinglePageProductById(Long id);
 }
