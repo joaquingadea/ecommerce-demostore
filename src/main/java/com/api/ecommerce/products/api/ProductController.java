0d94595dbd6f9c120a19d44750199492ee24e39a
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-25 21:23:50 -0300

    Creación del método getProducts con filtros personalizados.
    ProductController.java

--- src/main/java/com/api/ecommerce/products/api/ProductController.java
+++ src/main/java/com/api/ecommerce/products/api/ProductController.java
@@ -1,12 +1,18 @@
 package com.api.ecommerce.products.api;
 
 import com.api.ecommerce.products.application.IProductService;
-import com.api.ecommerce.products.dto.request.CreateProductRequestDTO;
+import com.api.ecommerce.products.dto.request.ProductFilter;
 import com.api.ecommerce.products.dto.response.ProductSinglePageDTO;
-import com.api.ecommerce.shared.web.ApiResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 
 @RestController
@@ -24,5 +30,16 @@ public class ProductController {
     public ResponseEntity<ProductSinglePageDTO> getById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productService.getSinglePageProductById(id));
+    }
+
+    @GetMapping("/get-products")
+    public ResponseEntity<Page<ProductSinglePageDTO>> getProducts(
+            @RequestParam(required = false) List<Long> categoryIds,
+            @RequestParam(required = false)BigDecimal minPrice,
+            @PageableDefault(size = 9, page = 0) Pageable pageable){
+        Pageable pageRequest = PageRequest.of(pageable.getPageNumber(),pageable.getPageSize());
+        ProductFilter filter = new ProductFilter(categoryIds,minPrice);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(productService.getProducts(filter,pageRequest));
     }
 }
