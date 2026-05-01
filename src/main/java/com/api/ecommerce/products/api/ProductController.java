fc91043d51e54a0c7931d6dbeeb5b98e33b35e54
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 21:25:05 -0300

    Creación del método getPublicProducts con filtros personalizados.
    ProductController.java

--- src/main/java/com/api/ecommerce/products/api/ProductController.java
+++ src/main/java/com/api/ecommerce/products/api/ProductController.java
@@ -2,17 +2,20 @@ package com.api.ecommerce.products.api;
 
 import com.api.ecommerce.products.application.IProductService;
 import com.api.ecommerce.products.dto.request.ProductFilter;
+import com.api.ecommerce.products.dto.response.AllDataProductDTO;
 import com.api.ecommerce.products.dto.response.ProductSinglePageDTO;
+import com.api.ecommerce.products.dto.response.PublicProductDTO;
+import com.api.ecommerce.products.infrastructure.persistence.sort.ProductSortBuilder;
+import com.api.ecommerce.shared.web.ApiResponse;
+import com.api.ecommerce.shared.web.PaginationConstants;
+import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 
 @RestController
@@ -32,14 +35,17 @@ public class ProductController {
                 .body(productService.getSinglePageProductById(id));
     }
 
-    @GetMapping("/get-products")
-    public ResponseEntity<Page<ProductSinglePageDTO>> getProducts(
-            @RequestParam(required = false) List<Long> categoryIds,
-            @RequestParam(required = false)BigDecimal minPrice,
-            @PageableDefault(size = 9, page = 0) Pageable pageable){
-        Pageable pageRequest = PageRequest.of(pageable.getPageNumber(),pageable.getPageSize());
-        ProductFilter filter = new ProductFilter(categoryIds,minPrice);
+    @GetMapping("/get")
+    public ResponseEntity<Page<PublicProductDTO>> getPublicProducts(
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) Long category,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) Boolean bestSellers,
+            @PageableDefault(size = 9, page = 0) Pageable pageable) throws BadRequestException {
+        Sort sortPageRequest = ProductSortBuilder.build(sort,bestSellers);
+        Pageable pageRequest = PaginationConstants.validatePageable(pageable.getPageNumber(),pageable.getPageSize(),sortPageRequest);
+        ProductFilter filter = new ProductFilter(search,category);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(productService.getProducts(filter,pageRequest));
+                .body(productService.getPublicProducts(filter,pageRequest));
     }
 }
