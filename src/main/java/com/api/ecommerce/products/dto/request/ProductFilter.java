4c66dd2fa5b069ab2b34883499f625c319de91b5
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 22:00:57 -0300

    Creación del DTO correspondiente para manejar los filtros de busqueda de productos.
    ProductFilter.java

--- src/main/java/com/api/ecommerce/products/dto/request/ProductFilter.java
+++ src/main/java/com/api/ecommerce/products/dto/request/ProductFilter.java
@@ -1,10 +1,7 @@
 package com.api.ecommerce.products.dto.request;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 public record ProductFilter(
-        List<Long> categoryIds,
-        BigDecimal minPrice
+        String search,
+        Long category
 ) {
 }
