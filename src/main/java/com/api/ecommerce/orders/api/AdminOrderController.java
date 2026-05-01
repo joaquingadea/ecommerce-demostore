542d053cfc335f7e3d3d7df1b8a1333953cbea09
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 21:21:05 -0300

    Creación del controller para manejo de endpoints propios de los administradores en el módulo orders.
    AdminOrderController.java

---
+++ src/main/java/com/api/ecommerce/orders/api/AdminOrderController.java
@@ -0,0 +1,9 @@
+package com.api.ecommerce.orders.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminOrderController {
+}
