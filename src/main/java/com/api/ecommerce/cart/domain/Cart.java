fb03fa25395d6015cc186a0cb21121baf70d6f50
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 23:55:22 -0300

    Se agrego el cascade ALL y el orphanRemoval = true en la relación 1 a N.
    Cart.java

--- src/main/java/com/api/ecommerce/cart/domain/Cart.java
+++ src/main/java/com/api/ecommerce/cart/domain/Cart.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
@@ -15,8 +16,8 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany(mappedBy = "cart")
-    private Set<CartItem> items;
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items = new HashSet<>();
     @OneToOne
     @JoinColumn(name = "user_id",unique = true)
     private AppUser user;
