9e13e1bdb2830336b5024b7961321437498e1e63
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 21:32:52 -0300

    Se agregó fetch LAZY y orphanRemoval = true a la relacion 1 a N de productos a imagenes y se implementó el atributo status.
    Product.java

--- src/main/java/com/api/ecommerce/products/domain/Product.java
+++ src/main/java/com/api/ecommerce/products/domain/Product.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
@@ -18,11 +19,14 @@ public class Product {
     private String description;
     private Integer stock;
     private BigDecimal unitPrice;
-    @OneToMany(mappedBy = "product",fetch = FetchType.LAZY)
-    private Set<ProductImage> images;
+    private Integer unitsSold;
+    @OneToMany(mappedBy = "product",fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<ProductImage> images;
     @ManyToMany
     @JoinTable(name = "product_categories",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<ProductCategory> productCategories;
+    private List<ProductCategory> productCategories;
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status;
 }
