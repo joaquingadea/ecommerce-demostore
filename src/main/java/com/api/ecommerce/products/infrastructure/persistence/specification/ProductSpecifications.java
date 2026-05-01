4ff3a797bc8ac57ff4fead32abb616e908731a79
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-30 22:23:54 -0300

    Creación de clase para manejo de specifications del módulo products.
    ProductSpecifications.java

--- src/main/java/com/api/ecommerce/products/infrastructure/persistence/specification/ProductSpecifications.java
+++ src/main/java/com/api/ecommerce/products/infrastructure/persistence/specification/ProductSpecifications.java
@@ -1,15 +1,61 @@
 package com.api.ecommerce.products.infrastructure.persistence.specification;
 
 import com.api.ecommerce.products.domain.Product;
+import com.api.ecommerce.products.domain.ProductCategory;
+import com.api.ecommerce.products.domain.ProductStatus;
+import jakarta.persistence.criteria.Join;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class ProductSpecifications {
 
-    public static Specification<Product> minPrice(BigDecimal min){
-        return (root, query, cb) ->
-                cb.greaterThanOrEqualTo(root.get("unitPrice"),min);
+    public static Specification<Product> isActive(){
+        return ((root, query, cb) ->
+                cb.equal(root.get("status"), ProductStatus.ACTIVE));
     }
+    public static Specification<Product> isInactive(){
+        return (root, query, cb) ->
+                cb.equal(root.get("status"),ProductStatus.INACTIVE);
+    }
+    public static Specification<Product> hasStock(){
+        return ((root, query, cb) ->
+                cb.greaterThan(root.get("stock"),BigDecimal.ZERO));
+    }
+    public static Specification<Product> withoutStock() {
+        return ((root, query, cb) ->
+                cb.lessThanOrEqualTo(root.get("stock"),BigDecimal.ZERO));
+    }
+    public static Specification<Product> nameContains(String keywords){
+        return ((root, query, cb) ->
+                cb.like(
+                        cb.lower(root.get("name"))
+                        ,"%" + keywords.toLowerCase() + "%"));
+    }
+    public static Specification<Product> hasCategories(List<Long> categoryIds) {
+        return (root, query, cb) -> {
+            Join<Product, ProductCategory> categoryJoin =
+                    root.join("productCategories");
+
+            query.distinct(true);
+
+            return categoryJoin.get("id").in(categoryIds);
+        };
+    }
+    public static Specification<Product> hasCategory(Long category) {
+        return (root, query, cb) -> {
+            Join<Product, ProductCategory> categoryJoin =
+                    root.join("productCategories");
+
+            query.distinct(true);
+
+            return cb.equal(
+                    categoryJoin.get("id"),
+                    category
+            );
+        };
+    }
+
 
 }
