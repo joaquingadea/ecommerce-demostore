38aa9834509e1621e1c7a07ef09e53d0b55fe6da
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-05-11 19:44:18 -0300

    Eliminación de dependencias innecesarias
    ProductService.java

--- src/main/java/com/api/ecommerce/products/application/ProductService.java
+++ src/main/java/com/api/ecommerce/products/application/ProductService.java
@@ -24,11 +24,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,7 +31,7 @@ import java.util.List;
 @Transactional
 public class ProductService implements IProductService{
 
-    @Value("app.url")
+    @Value("${app.url}")
     private String urlApp;
 
     private IProductRepository productRepository;
