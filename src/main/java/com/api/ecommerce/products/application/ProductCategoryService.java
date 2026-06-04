4911bd6581697e10a444625983ca27a549b8370e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-04 04:09:10 -0300

    Creación de los metodos getAll y getCategoryList
    ProductCategoryService.java

--- src/main/java/com/api/ecommerce/products/application/ProductCategoryService.java
+++ src/main/java/com/api/ecommerce/products/application/ProductCategoryService.java
@@ -2,7 +2,11 @@ package com.api.ecommerce.products.application;
 
 import com.api.ecommerce.products.domain.ProductCategory;
 import com.api.ecommerce.products.infrastructure.persistence.IProductCategoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductCategoryService implements IProductCategoryService{
@@ -15,7 +19,7 @@ public class ProductCategoryService implements IProductCategoryService{
 
     @Override
     public void create(String requestCategory) {
-        ProductCategory newCategory = new ProductCategory(1L, requestCategory);
+        ProductCategory newCategory = new ProductCategory(null, requestCategory);
         productCategoryRepository.save(newCategory);
     }
 
@@ -24,5 +28,15 @@ public class ProductCategoryService implements IProductCategoryService{
         ProductCategory productCategoryRepo = productCategoryRepository.findById(id).orElseThrow();
         productCategoryRepo.setName(newName);
         productCategoryRepository.save(productCategoryRepo);
+    }
+
+    @Override
+    public Page<ProductCategory> getAll(Pageable pageRequest) {
+        return productCategoryRepository.findAll(pageRequest);
+    }
+
+    @Override
+    public List<ProductCategory> getCategoryList() {
+        return productCategoryRepository.findAll();
     }
 }
