bf2572431c29c48d75eea16ab1b8cb96a3cddf22
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-04 04:23:56 -0300

    Definición de método getAll y getCategory en el contrato.
    IProductCategoryService.java

--- src/main/java/com/api/ecommerce/products/application/IProductCategoryService.java
+++ src/main/java/com/api/ecommerce/products/application/IProductCategoryService.java
@@ -1,6 +1,15 @@
 package com.api.ecommerce.products.application;
 
+import com.api.ecommerce.products.domain.ProductCategory;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface IProductCategoryService {
     void create(String requestCategory);
     void edit(Long id, String newName);
+    Page<ProductCategory> getAll(Pageable pageRequest);
+    List<ProductCategory> getCategoryList();
 }
