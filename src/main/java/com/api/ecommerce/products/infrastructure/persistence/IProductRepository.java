4a0a27df014584a2a631fca6f5336d7225c1430e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-04 02:32:22 -0300

    Se agregó el método findAllDataProjectedById y se le agregó la extension de JpaSpecificationExecutor a la interfaz.
    IProductRepository.java

--- src/main/java/com/api/ecommerce/products/infrastructure/persistence/IProductRepository.java
+++ src/main/java/com/api/ecommerce/products/infrastructure/persistence/IProductRepository.java
@@ -1,9 +1,14 @@
 package com.api.ecommerce.products.infrastructure.persistence;
 
 import com.api.ecommerce.products.domain.Product;
+import com.api.ecommerce.products.dto.response.AllDataProductDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface IProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {}
+public interface IProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
+    Optional<AllDataProductDTO> findAllDataProjectedById(Long id);
+}
