ac2552b5b8cf9b55053faca453c2a77842d4b3b7
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-04 01:53:10 -0300

    Creación del método decodeJwt
    JwtService.java

--- src/main/java/com/api/ecommerce/shared/security/jwt/JwtService.java
+++ src/main/java/com/api/ecommerce/shared/security/jwt/JwtService.java
@@ -2,12 +2,15 @@ package com.api.ecommerce.shared.security.jwt;
 
 import com.api.ecommerce.users.infrastructure.security.CustomUserDetails;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -20,9 +23,8 @@ public class JwtService {
     @Value("${jwt.user.generator}")
     private String userGenerator;
 
-    private Algorithm algorithm = Algorithm.HMAC256(privateKey);
-
     public String createToken(Authentication authentication){
+        Algorithm algorithm = Algorithm.HMAC256(privateKey);
 
         CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
 
@@ -37,5 +39,22 @@ public class JwtService {
                 .withJWTId(UUID.randomUUID().toString())
                 .withIssuer(userGenerator)
                 .sign(algorithm);
+    }
+
+    public DecodedJWT decodeJwt(String jwt) {
+        Algorithm algorithm = Algorithm.HMAC256(privateKey);
+        try {
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(userGenerator)
+                    .build();
+            return verifier.verify(jwt);
+        }
+        catch (JWTVerificationException e){
+            throw new JWTVerificationException("Invalid token!");
+        }
+    }
+
+    public Claim getSpecificClaim(String name,DecodedJWT decodedJWT){
+        return decodedJWT.getClaim(name);
     }
 }
