e25144fc78e2b863a1d481d2c29a4b6bc4a49bab
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-09 23:39:55 -0300

    Eliminación de las annotations para las validaciones del DTO (las validaciones quedan en los parametros del método)
    CreateProductDTO.java

--- src/main/java/com/api/ecommerce/products/dto/request/CreateProductDTO.java
+++ src/main/java/com/api/ecommerce/products/dto/request/CreateProductDTO.java
@@ -7,17 +7,11 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public record CreateProductDTO(
-        @NotEmpty
         String name,
-        @NotEmpty
         String description,
-        @Min(value = 1)
         BigDecimal price,
-        @Min(value = 0)
         Integer stock,
-        @Size(min = 1, max = 6)
         List<MultipartFile> images,
-        @Size(min = 1)
         List<Long> categories
 ) {
 }
