4b5b7f8e972dda1b85bfab32d2f2870cfa79bae9
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-16 21:23:28 -0300

    Creación del método findByUserId
    ICartRepository.java

--- src/main/java/com/api/ecommerce/cart/infrastructure/ICartRepository.java
+++ src/main/java/com/api/ecommerce/cart/infrastructure/ICartRepository.java
@@ -4,6 +4,9 @@ import com.api.ecommerce.cart.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ICartRepository extends JpaRepository<Cart,Long> {
+    Optional<Cart> findByUserId(Long userId);
 }
