86e15ad3579d29fee4e1b46696d45c9e2ce19573
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-19 00:32:25 -0300

    Agregado de atributos orderDetailId, description y firstImageUrl
    PaymentItemDTO.java

--- src/main/java/com/api/ecommerce/payments/dto/request/PaymentItemDTO.java
+++ src/main/java/com/api/ecommerce/payments/dto/request/PaymentItemDTO.java
@@ -3,8 +3,11 @@ package com.api.ecommerce.payments.dto.request;
 import java.math.BigDecimal;
 
 public record PaymentItemDTO(
+        String orderDetailId,
         String name,
+        String description,
         Integer quantity,
+        String firstImageUrl,
         BigDecimal unitPrice
 ) {
 }
