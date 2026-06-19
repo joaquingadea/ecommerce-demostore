050cf01a5480fe947c08d41c05d758d51b82b25d
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-18 23:11:21 -0300

    Creación e implementación del método createOrderFromCart
    OrderService y IOrderService

--- src/main/java/com/api/ecommerce/orders/application/IOrderService.java
+++ src/main/java/com/api/ecommerce/orders/application/IOrderService.java
@@ -1,4 +1,8 @@
 package com.api.ecommerce.orders.application;
 
+import com.api.ecommerce.cart.domain.Cart;
+import com.api.ecommerce.orders.domain.Order;
+
 public interface IOrderService {
+    Order createOrderFromCart(Cart cart);
 }
--- src/main/java/com/api/ecommerce/orders/application/OrderService.java
+++ src/main/java/com/api/ecommerce/orders/application/OrderService.java
@@ -1,7 +1,51 @@
 package com.api.ecommerce.orders.application;
 
+import com.api.ecommerce.cart.domain.Cart;
+import com.api.ecommerce.orders.domain.Order;
+import com.api.ecommerce.orders.domain.OrderDetail;
+import com.api.ecommerce.orders.domain.OrderStatus;
+import com.api.ecommerce.orders.infrastructure.persistence.IOrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class OrderService implements IOrderService{
+
+    private IOrderRepository orderRepository;
+
+    public OrderService(IOrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order createOrderFromCart(Cart cart) {
+        Order order = new Order();
+        List<OrderDetail> orderDetails = cart.getItems().stream()
+                .map(cartItem ->
+                        new OrderDetail(
+                                null,
+                                cartItem.getQuantity(),
+                                cartItem.getProduct().getUnitPrice(),
+                                cartItem.getProduct().getUnitPrice().multiply(BigDecimal.valueOf(cartItem.getQuantity())),
+                                order,
+                                cartItem.getProduct()
+                        )
+                )
+                .toList();
+
+        BigDecimal total = orderDetails.stream()
+                .map(OrderDetail::getSubtotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        order.setTotal(total);
+        order.setOrderDetails(orderDetails);
+        order.setStatus(OrderStatus.PENDING);
+        order.setCreatedAt(LocalDateTime.now());
+        orderRepository.save(order);
+        return order;
+    }
+
 }
