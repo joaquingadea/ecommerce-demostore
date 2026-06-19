35e99c2b97a9e6a14b2b3cff1b43746b6df31110
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-19 00:31:35 -0300

    Eliminación de importaciones innecesarias.
    CreatePaymentDTO.java

--- src/main/java/com/api/ecommerce/payments/dto/request/CreatePaymentDTO.java
+++ src/main/java/com/api/ecommerce/payments/dto/request/CreatePaymentDTO.java
@@ -1,12 +1,11 @@
 package com.api.ecommerce.payments.dto.request;
 
-import com.mercadopago.resources.payment.PaymentItem;
 
 import java.util.List;
 
 public record CreatePaymentDTO(
         Long paymentId,
-        List<PaymentItem> items,
+        List<PaymentItemDTO> items,
         String successUrl,
         String pendingUrl,
         String failureUrl
