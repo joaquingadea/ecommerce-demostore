ad34901f276324fda62752e057622002eb561f66
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-20 22:26:18 -0300

    Creación y avance con la implementación del método process
    MercadoPagoWebhookService.java

---
+++ src/main/java/com/api/ecommerce/payments/infrastructure/mercadopago/MercadoPagoWebhookService.java
@@ -0,0 +1,36 @@
+package com.api.ecommerce.payments.infrastructure.mercadopago;
+
+import com.api.ecommerce.orders.application.IOrderService;
+import com.api.ecommerce.payments.domain.PaymentGateway;
+import com.api.ecommerce.payments.domain.PaymentStatus;
+import com.api.ecommerce.payments.dto.response.PaymentDetailsDTO;
+import com.api.ecommerce.payments.infrastructure.mercadopago.dto.request.MercadoPagoNotificationDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MercadoPagoWebhookService {
+
+    private final PaymentGateway paymentGateway;
+    private final IOrderService orderService;
+
+    public MercadoPagoWebhookService(PaymentGateway paymentGateway, IOrderService orderService) {
+        this.paymentGateway = paymentGateway;
+        this.orderService = orderService;
+    }
+
+    public void process(MercadoPagoNotificationDTO notification) {
+
+        if ("payment".equals(notification.getType())) {
+
+            String paymentId = notification.getData().getId();
+
+            PaymentDetailsDTO payment = paymentGateway.getPayment(paymentId);
+
+            if (payment.status().equals(PaymentStatus.APPROVED)) {
+                //orderService.markAsPaid(payment);
+            }
+        }
+    }
+}
