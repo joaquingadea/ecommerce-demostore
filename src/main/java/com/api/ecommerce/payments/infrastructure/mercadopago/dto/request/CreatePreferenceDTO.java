d3d0bb11720d346caeb9c74f07ab65f5b28ee59b
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-20 22:32:19 -0300

    Creación de dto correspondiente para el manejo de la creación de preference dentro del contexto de mercadopago
    CreatePreferenceDTO.java

---
+++ src/main/java/com/api/ecommerce/payments/infrastructure/mercadopago/dto/request/CreatePreferenceDTO.java
@@ -0,0 +1,13 @@
+package com.api.ecommerce.payments.infrastructure.mercadopago.dto.request;
+
+import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
+import com.mercadopago.client.preference.PreferenceItemRequest;
+
+import java.util.List;
+
+public record CreatePreferenceDTO(
+        String externalReference, // externo para la API de mercado pago (orderId)
+        List<PreferenceItemRequest> items,
+        PreferenceBackUrlsRequest backUrls
+) {
+}
