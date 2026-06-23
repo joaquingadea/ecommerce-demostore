2899f4686767d22455659aab012904f189ebc32e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-23 19:07:00 -0300

    Creación del método findByUserIdAndStatus
    IOrderRepository

--- src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderRepository.java
+++ src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderRepository.java
@@ -1,9 +1,13 @@
 package com.api.ecommerce.orders.infrastructure.persistence;
 
 import com.api.ecommerce.orders.domain.Order;
+import com.api.ecommerce.orders.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IOrderRepository extends JpaRepository<Order,Long> {
+    Optional<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
 }
