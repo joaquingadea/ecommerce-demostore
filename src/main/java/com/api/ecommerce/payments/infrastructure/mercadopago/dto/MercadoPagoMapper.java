a886d68f96163ee4a562d632546280f7a56e97e5
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-22 23:56:42 -0300

    Creación de los métodos mapStatus y toExternalPaymentDetailsDTO
    MercadoPagoMapper.java

--- src/main/java/com/api/ecommerce/payments/infrastructure/mercadopago/dto/MercadoPagoMapper.java
+++ src/main/java/com/api/ecommerce/payments/infrastructure/mercadopago/dto/MercadoPagoMapper.java
@@ -1,13 +1,18 @@
 package com.api.ecommerce.payments.infrastructure.mercadopago.dto;
 
+import com.api.ecommerce.payments.domain.PaymentStatus;
 import com.api.ecommerce.payments.dto.request.CreatePaymentDTO;
+import com.api.ecommerce.payments.dto.response.ExternalPaymentDetailsDTO;
 import com.api.ecommerce.payments.infrastructure.mercadopago.dto.request.CreatePreferenceDTO;
 import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
 import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.resources.payment.Payment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
 
 @Component
 public class MercadoPagoMapper {
@@ -15,7 +20,35 @@ public class MercadoPagoMapper {
     @Value("${app.url}")
     private String urlBackend;
 
-    public CreatePreferenceDTO toPreference(CreatePaymentDTO dto){
+    public PaymentStatus mapStatus(String mpStatus) {
+        if (mpStatus == null) {
+            return PaymentStatus.PENDING;
+        }
+        return switch (mpStatus.toLowerCase(Locale.ROOT)) {
+
+            case "approved" -> PaymentStatus.APPROVED;
+            case "rejected","cancelled" -> PaymentStatus.REJECTED;
+            case "in_process","pending", "authorized" -> PaymentStatus.PENDING;
+
+            default -> throw new IllegalArgumentException(
+                    "Unknown MercadoPago status: " + mpStatus
+            );
+        };
+    }
+
+    public ExternalPaymentDetailsDTO toExternalPaymentDetailsDTO(Payment paymentMP){
+        return new ExternalPaymentDetailsDTO(
+                paymentMP.getId().toString(),
+                mapStatus(paymentMP.getStatus()),
+                paymentMP.getTransactionAmount(),
+                paymentMP.getExternalReference(),
+                paymentMP.getPaymentTypeId(),
+                paymentMP.getDateCreated().toLocalDateTime(),
+                paymentMP.getDateApproved().toLocalDateTime()
+        );
+    }
+
+    public CreatePreferenceDTO toCreatePreferenceDTO(CreatePaymentDTO dto){
 
         PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
                 .success(dto.successUrl())
