d20508f2d15b6c643eef188ee7467f68cbc8ef03
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-23 19:05:57 -0300

    Creación e implementación de los métodos markAsPaid, markAsRejected y getOrCreateAttemptPayment
    PaymentService y IPaymentService

--- src/main/java/com/api/ecommerce/payments/application/IPaymentService.java
+++ src/main/java/com/api/ecommerce/payments/application/IPaymentService.java
@@ -3,6 +3,10 @@ package com.api.ecommerce.payments.application;
 import com.api.ecommerce.orders.domain.Order;
 import com.api.ecommerce.payments.domain.Payment;
 
+import java.util.Optional;
+
 public interface IPaymentService {
-    Payment createAttemptPayment(Order order);
+    void markAsPaid(Long paymentId);
+    void markAsRejected(Long paymentId);
+    Payment getOrCreateAttemptPayment(Order order);
 }
--- src/main/java/com/api/ecommerce/payments/application/PaymentService.java
+++ src/main/java/com/api/ecommerce/payments/application/PaymentService.java
@@ -1,23 +1,26 @@
 package com.api.ecommerce.payments.application;
 
 import com.api.ecommerce.orders.domain.Order;
+import com.api.ecommerce.orders.domain.OrderStatus;
 import com.api.ecommerce.payments.domain.Payment;
 import com.api.ecommerce.payments.domain.PaymentStatus;
 import com.api.ecommerce.payments.infrastructure.persistence.IPaymentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @Service
+@Transactional
 public class PaymentService implements IPaymentService {
 
     private IPaymentRepository paymentRepository;
+
 
     public PaymentService(IPaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
     }
 
-    @Override
     public Payment createAttemptPayment(Order order){
         Payment payment = new Payment(
                 null,
@@ -30,5 +33,34 @@ public class PaymentService implements IPaymentService {
         );
         paymentRepository.save(payment);
         return payment;
+    }
+
+    @Override
+    public void markAsPaid(Long paymentId) {
+        Payment paymentRepo = paymentRepository.findById(paymentId).orElseThrow();
+        paymentRepo.setStatus(PaymentStatus.APPROVED);
+        paymentRepo.getOrder().setStatus(OrderStatus.PAID);
+    }
+
+    @Override
+    public void markAsRejected(Long paymentId) {
+        Payment paymentRepo = paymentRepository.findById(paymentId).orElseThrow();
+        paymentRepo.setStatus(PaymentStatus.REJECTED);
+    }
+
+    @Override
+    public Payment getOrCreateAttemptPayment(Order order){
+
+        boolean approvedPayment = order.getPaymentAttempts().stream()
+                .anyMatch(payment -> payment.getStatus().equals(PaymentStatus.APPROVED));
+
+        if (approvedPayment) {
+            throw new IllegalStateException("Payment already paid!");
+        }
+
+        return order.getPaymentAttempts().stream()
+                .filter(payment -> payment.getStatus().equals(PaymentStatus.PENDING))
+                .findFirst()
+                .orElseGet(() -> createAttemptPayment(order));
     }
 }
