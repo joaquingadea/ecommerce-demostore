ec3105cd84f28d74cf5ab4a52dc91619e31f8cb5
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-23 21:07:31 -0300

    Se estableció como final el atributo paymentRepository
    PaymentService

--- src/main/java/com/api/ecommerce/payments/application/PaymentService.java
+++ src/main/java/com/api/ecommerce/payments/application/PaymentService.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Transactional
 public class PaymentService implements IPaymentService {
 
-    private IPaymentRepository paymentRepository;
+    private final IPaymentRepository paymentRepository;
 
 
     public PaymentService(IPaymentRepository paymentRepository) {
