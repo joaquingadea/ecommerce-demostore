0397a417f118d284685860129df4f7e7f378fe63
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-30 18:22:47 -0300

    Modificación en el método getCustomers
    AdminUserController

--- src/main/java/com/api/ecommerce/users/api/AdminUserController.java
+++ src/main/java/com/api/ecommerce/users/api/AdminUserController.java
@@ -1,17 +1,20 @@
 package com.api.ecommerce.users.api;
 
+import com.api.ecommerce.shared.security.jwt.JwtPrincipal;
 import com.api.ecommerce.users.application.IAppUserService;
 import com.api.ecommerce.users.application.IPermissionService;
 import com.api.ecommerce.users.application.IRoleService;
 import com.api.ecommerce.users.domain.Permission;
 import com.api.ecommerce.users.domain.Role;
-import com.api.ecommerce.users.dto.response.UserIdUsernameDTO;
+import com.api.ecommerce.users.dto.response.UserIdUsernameRoleDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,10 +34,11 @@ public class AdminUserController {
     }
 
     @GetMapping("/customer-list")
-    public ResponseEntity<Page<UserIdUsernameDTO>> getCustomers(@PageableDefault(size = 10,page = 0) Pageable pageable){
+    public ResponseEntity<Page<UserIdUsernameRoleDTO>> getCustomers(@PageableDefault(size = 10,page = 0) Pageable pageable,
+                                                                    @AuthenticationPrincipal JwtPrincipal auth){
         Pageable pageRequest = PageRequest.of(pageable.getPageNumber(),pageable.getPageSize());
         return ResponseEntity.status(HttpStatus.OK)
-                .body(appUserService.findAllIdAndUsername(pageRequest));
+                .body(appUserService.findAllIdAndUsernameAndRole(pageRequest,auth.userId()));
     }
 
 
