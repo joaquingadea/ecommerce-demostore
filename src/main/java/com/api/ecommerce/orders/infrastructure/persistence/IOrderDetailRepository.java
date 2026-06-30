a89598cdb8f7cbc1979cad1eae891b0e3e77fc07
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-30 17:55:28 -0300

    Agregado del método para encontrar los detalles de una orden por el id de la misma
    IOrderDetailRepository

--- src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderDetailRepository.java
+++ src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderDetailRepository.java
@@ -1,9 +1,25 @@
 package com.api.ecommerce.orders.infrastructure.persistence;
 
 import com.api.ecommerce.orders.domain.OrderDetail;
+import com.api.ecommerce.orders.dto.response.MyOrderDetailsDTO;
+import com.api.ecommerce.orders.dto.response.OrderDetailDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IOrderDetailRepository extends JpaRepository<OrderDetail,Long> {
+    @Query("""
+    SELECT
+        od.quantity AS quantity,
+        od.subtotal AS subtotal,
+        od.unitPrice AS unitPrice,
+        od.product.name AS productName
+        FROM OrderDetail od
+        WHERE od.order.id = :orderId
+    """)
+    List<OrderDetailDTO> findByOrderId(@Param("orderId")Long orderId);
 }
