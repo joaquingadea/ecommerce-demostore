7baa2ed9a26e170198e06de4998cf23371949e5a
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-30 17:58:42 -0300

    Agregado del método findAllProjectedBy
    IOrderRepository

--- src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderRepository.java
+++ src/main/java/com/api/ecommerce/orders/infrastructure/persistence/IOrderRepository.java
@@ -2,6 +2,10 @@ package com.api.ecommerce.orders.infrastructure.persistence;
 
 import com.api.ecommerce.orders.domain.Order;
 import com.api.ecommerce.orders.domain.OrderStatus;
+import com.api.ecommerce.orders.dto.response.MyOrderDTO;
+import com.api.ecommerce.orders.dto.response.OrderDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +14,5 @@ import java.util.Optional;
 @Repository
 public interface IOrderRepository extends JpaRepository<Order,Long> {
     Optional<Order> findByUserIdAndStatus(Long userId, OrderStatus status);
+    Page<OrderDTO> findAllProjectedBy(Pageable ordersPageable);
 }
