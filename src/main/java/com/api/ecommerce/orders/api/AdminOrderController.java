e3f2596c96ed284e60e95ff051268bdc3ac5c66b
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-30 17:17:07 -0300

    Iny de dependencias de IOrderService
    AdminOrderController

--- src/main/java/com/api/ecommerce/orders/api/AdminOrderController.java
+++ src/main/java/com/api/ecommerce/orders/api/AdminOrderController.java
@@ -1,9 +1,29 @@
 package com.api.ecommerce.orders.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.ecommerce.orders.application.IOrderService;
+import com.api.ecommerce.orders.dto.response.OrderDTO;
+import com.api.ecommerce.orders.dto.response.OrderDetailDTO;
+import com.api.ecommerce.orders.infrastructure.persistence.sort.OrderSortBuilder;
+import com.api.ecommerce.shared.web.PaginationConstants;
+import org.apache.coyote.BadRequestException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/orders")
 public class AdminOrderController {
+
+    private final IOrderService orderService;
+
+    public AdminOrderController(IOrderService orderService) {
+        this.orderService = orderService;
+    }
+
 }
