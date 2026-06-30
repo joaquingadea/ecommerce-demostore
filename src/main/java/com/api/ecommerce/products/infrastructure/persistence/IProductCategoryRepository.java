f141d264d593b64feab607313fa7ce34d87acaba
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-30 18:13:30 -0300

    Agregado del método findAllPublicCategories
    IProductCategoryRepository

--- src/main/java/com/api/ecommerce/products/infrastructure/persistence/IProductCategoryRepository.java
+++ src/main/java/com/api/ecommerce/products/infrastructure/persistence/IProductCategoryRepository.java
@@ -1,9 +1,15 @@
 package com.api.ecommerce.products.infrastructure.persistence;
 
 import com.api.ecommerce.products.domain.ProductCategory;
+import com.api.ecommerce.products.dto.response.PublicCategoryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+    @Query(" SELECT c.id AS id, c.name AS name FROM ProductCategory c ")
+    List<PublicCategoryDTO> findAllPublicCategories();
 }
