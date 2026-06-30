beaf65944bed5db8847506aef82636459c586d7f
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-06-30 18:26:39 -0300

    Agregado de los métodos setAdmin, revokeAdmin y findAllIdAndUsernameAndRole en el contrato
    IAppUserService

--- src/main/java/com/api/ecommerce/users/application/IAppUserService.java
+++ src/main/java/com/api/ecommerce/users/application/IAppUserService.java
@@ -5,15 +5,16 @@ import com.api.ecommerce.users.dto.request.LoginRequestDTO;
 import com.api.ecommerce.users.dto.request.RegisterRequestDTO;
 import com.api.ecommerce.users.dto.response.LoginResponseDTO;
 import com.api.ecommerce.users.dto.response.RegisterResponseDTO;
-import com.api.ecommerce.users.dto.response.UserIdUsernameDTO;
+import com.api.ecommerce.users.dto.response.UserIdUsernameRoleDTO;
 import jakarta.servlet.http.HttpServletResponse;
-import org.jspecify.annotations.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface IAppUserService {
-    Page<UserIdUsernameDTO> findAllIdAndUsername(Pageable pageRequest);
+    Page<UserIdUsernameRoleDTO> findAllIdAndUsernameAndRole(Pageable pageRequest,Long adminId);
     LoginResponseDTO loginUser(LoginRequestDTO request, HttpServletResponse response);
     RegisterResponseDTO registerUser(RegisterRequestDTO request);
     ApiResponse logoutUser(HttpServletResponse response);
+    void setAdmin(Long userId);
+    void revokeAdmin(Long userId);
 }
