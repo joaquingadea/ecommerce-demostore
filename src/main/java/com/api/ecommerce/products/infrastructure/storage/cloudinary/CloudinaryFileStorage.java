ab6fc718c85d8ac3da4d2d3281dc0c7a632f39ea
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-07-01 20:47:26 -0300

    Implementación de los métodos del contrato FileStorage
    CloudinaryFileStorage

--- src/main/java/com/api/ecommerce/products/infrastructure/storage/cloudinary/CloudinaryFileStorage.java
+++ src/main/java/com/api/ecommerce/products/infrastructure/storage/cloudinary/CloudinaryFileStorage.java
@@ -1,10 +1,16 @@
 package com.api.ecommerce.products.infrastructure.storage.cloudinary;
 
 import com.api.ecommerce.products.domain.FileStorage;
+import com.api.ecommerce.products.domain.ImageData;
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @ConditionalOnProperty(
@@ -14,23 +20,40 @@ import org.springframework.web.multipart.MultipartFile;
 public class CloudinaryFileStorage implements FileStorage {
 
     private final Cloudinary cloudinary;
-
+    private static final String PRODUCTS_FOLDER = "products";
     public CloudinaryFileStorage(Cloudinary cloudinary) {
         this.cloudinary = cloudinary;
     }
 
     @Override
-    public String upload(MultipartFile file) {
-        return "";
+    public ImageData upload(MultipartFile file) {
+        try {
+            Map<String, Object> options = new HashMap<>();
+            options.put("folder", PRODUCTS_FOLDER);
+
+
+            Map<?, ?> result = cloudinary.uploader()
+                    .upload(
+                            file.getBytes(),
+                            options
+                    );
+
+            return new ImageData(
+                    result.get("public_id").toString(),
+                    result.get("secure_url").toString()
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void delete(String fileId) {
-
-    }
-
-    @Override
-    public String getUrl(String fileId) {
-        return "";
+    public void delete(String publicId) {
+        try {
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
