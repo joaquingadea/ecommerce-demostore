89f17e11cfb08e909bca736d46f610ec0db6cbc7
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-07-01 02:28:19 -0300

    Creación del contrato FileStorageService

---
+++ src/main/java/com/api/ecommerce/products/domain/FileStorageService.java
@@ -0,0 +1,9 @@
+package com.api.ecommerce.products.domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+    String upload(MultipartFile file);
+    void delete(String externalFileId);
+    String getUrl(String externalFileId);
+}
