086659cd85cd7ae2c780987bf8b430c03acf7072
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-07-01 20:46:25 -0300

    Eliminacion de getUrl del contrato
    FileStorage

--- src/main/java/com/api/ecommerce/products/domain/FileStorage.java
+++ src/main/java/com/api/ecommerce/products/domain/FileStorage.java
@@ -5,5 +5,4 @@ import org.springframework.web.multipart.MultipartFile;
 public interface FileStorage {
     ImageData upload(MultipartFile file);
     void delete(String externalFileId);
-    String getUrl(String externalFileId);
 }
